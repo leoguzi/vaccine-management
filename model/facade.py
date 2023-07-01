2a7196ef72546bfcf0473f067f8b95a0898291a8
Ruan Pablo <ruestacio@gmail.com>
2023-07-01 11:09:21 -0300

    ok - metodos de EnfDAO e T.enf

--- model/facade.py
+++ model/facade.py
@@ -13,23 +13,26 @@ class Facade:
     def get_instaciar_enfermeiro(self, nome, codigo):
         return Enfermeiro(nome, codigo)
     
+    def get_instaciar_tela_enfermeiro():
+        return TelaEnfermeiros()
+    
     def adiciona_enfermeiro(self, enfermeiro: Enfermeiro):
-        self.__enfermeiro_dao.add(self, enfermeiro)
+        self.__enfermeiro_dao.add(enfermeiro)
     
     def pega_enfermeiro(self, key:int):
-        return self.__enfermeiro_dao.get(self, key)
+        return self.__enfermeiro_dao.get(key)
     
     def remove_enfermeiro(self, key:int):
-        return self.__enfermeiro_dao.remove(self, key)
+        return self.__enfermeiro_dao.remove(key)
 
     def atualiza_enfermeiro(self):
-        self.__enfermeiro_dao.update(self)
+        self.__enfermeiro_dao.update()
 
     def pega_tudo_enfermeiro(self):
-        self.__enfermeiro_dao.get_all()
+        return self.__enfermeiro_dao.get_all()
     ##### METODOS de TelaEnfermeiro 
     def opcoes_enfermeiro_tela(self):
-        return self.__tela_enfermeiros(self)
+        return self.__tela_enfermeiros.opcoes_enfermeiro()
     
     def le_nome_tela(self, nome_atual = None):
         return self.__tela_enfermeiros.le_nome(nome_atual)
@@ -37,7 +40,7 @@ class Facade:
     def mostra_enfermeiro_tela(self, lista_enfermeiros):
         self.__tela_enfermeiros.mostra_enfermeiros(lista_enfermeiros)
     
-    def combo_box_enfermeiros(self, lista_enfermeiros):
+    def combo_box_enfermeiros_tela(self, lista_enfermeiros):
         return self.__tela_enfermeiros.combo_box_enfermeiros(lista_enfermeiros)
     
     def mensagem_tela_enfermeiro(self, mensagem: str):
